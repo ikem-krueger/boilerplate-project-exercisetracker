0b698558bb5057a296731e51f2f2ae78788897fa
Ikem Krueger <ikem.krueger@gmail.com>
2023-09-23 01:24:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,93 +13,33 @@ app.get('/', (req, res) => {
 
 app.use(express.urlencoded({ extended: true }));
 
-const { customAlphabet } = require("fix-esm").require('nanoid');
+const { customAlphabet } = require('fix-esm').require('nanoid');
 
-const nanoid = customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 24);
-
-const users = [{ username: "fcc_test", _id: "5fb5853f734231456ccb3b05" }]; // TODO: write real "data storage" logic?
-const exercises = [];
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 24);
 
 /*
- - [x] You can POST to /api/users with form data username to create a new user.
- - [x] The returned response from POST /api/users with form data username will be an object with username and _id properties.
-*/
-app.post('/api/users', (req, res) => {
-  const user = { username: req.body.username, _id: nanoid() };
-
-  users.push(user); // TODO: write logic to catch errors if the action fails
-
-  res.json(user);
-});
-
-/*
- - [-] You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
- - [-] The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
-*/
-app.post('/api/users/:_id/exercises', (req, res) => {
-  const _id = req.params._id;
-
-  const index = users.findIndex(user => user._id === _id);
-
-  const user = users[index];
-
-  let { description, duration, date } = req.body;
-
-  date = date || new Date();
-
-  const exercise = { ...user, description, duration, date };
-
-  exercises[index] = exercise; // TODO: safe changes properly
-
-  res.json(exercise);
-});
-
-/*
- - [x] You can make a GET request to /api/users to get a list of all users.
- - [x] The GET request to /api/users returns an array.
- - [x] Each element in the array returned from GET /api/users is an object literal containing a user's username and _id.
-*/
-app.get('/api/users', (req, res) => {
-  res.json(users);
-});
-
-/*
- - [ ] You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
- - [ ] A request to a user's log GET /api/users/:_id/logs returns a user object with a count property representing the number of exercises that belong to that user.
- - [ ] A GET request to /api/users/:_id/logs will return the user object with a log array of all the exercises added.
- - [ ] Each item in the log array that is returned from GET /api/users/:_id/logs is an object that should have a description, duration, and date properties.
- - [ ] The description property of any object in the log array that is returned from GET /api/users/:_id/logs should be a string.
- - [ ] The duration property of any object in the log array that is returned from GET /api/users/:_id/logs should be a number.
- - [ ] The date property of any object in the log array that is returned from GET /api/users/:_id/logs should be a string. Use the dateString format of the Date API.
- - [ ] You can add from, to and limit parameters to a GET /api/users/:_id/logs request to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. limit is an integer of how many logs to send back.
-
- - [ ] You should provide your own project, not the example URL.
-*/
-
-/*
-  Exercise:
-
-  {
-    username: "fcc_test",
-    description: "test",
-    duration: 60,
-    date: "Mon Jan 01 1990",
-    _id: "5fb5853f734231456ccb3b05"
-  }
-
   User:
 
   {
-    username: "fcc_test",
     _id: "5fb5853f734231456ccb3b05"
+    username: "fcc_test",
+  }
+
+  Exercise:
+
+  {
+    _id: "5fb5853f734231456ccb3b05"
+    description: "test",
+    duration: 60,
+    date: "Mon Jan 01 1990",
   }
 
   Log:
 
   {
+    _id: "5fb5853f734231456ccb3b05",
     username: "fcc_test",
     count: 1,
-    _id: "5fb5853f734231456ccb3b05",
     log: [{
       description: "test",
       duration: 60,
@@ -107,6 +47,86 @@ app.get('/api/users', (req, res) => {
     }]
   }
 */
+const users = [];
+const exercises = [];
+
+function getUser(id) {
+  return users.find(({ _id }) => _id == id);
+}
+
+function createLog(id) {
+  return exercises.filter(({ _id }) => _id == id).map(({ description, duration, date }) => ({ description, duration, date }));
+}
+
+/*
+ - [x] You can POST to /api/users with form data username to create a new user.
+ - [x] The returned response from POST /api/users with form data username will be an object with username and _id properties.
+*/
+app.post('/api/users', (req, res) => {
+  const _id = nanoid(); // create new id
+  const { username } = req.body;
+
+  const user = { _id, username }; // create new user
+
+  users.push(user); // add user to users
+
+  res.json(user);
+});
+
+/*
+ - [x] You can make a GET request to /api/users to get a list of all users.
+ - [x] The GET request to /api/users returns an array.
+ - [x] Each element in the array returned from GET /api/users is an object literal containing a user's _id and username.
+*/
+app.get('/api/users', (req, res) => {
+  res.json(users); // get all users
+});
+
+/*
+ - [x] You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
+ - [x] The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
+*/
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const { _id } = req.params;
+
+  let { description, duration, date } = req.body;
+
+  duration = parseInt(duration);
+  date = date || new Date();
+
+  const exercise = { _id, description, duration, date }; // create new exercise
+
+  exercises.push(exercise); // add exercise to exercises
+
+  const user = getUser(_id);
+  const log = createLog(_id);
+
+  res.json({ ...user, count: log.length, log });
+});
+
+/*
+ - [x] A GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
+ - [x] A GET request to /api/users/:_id/logs returns a user object with a count property representing the number of exercises that belong to that user.
+ - [x] A GET request to /api/users/:_id/logs will return the user object with a log array of all the exercises added.
+
+ - [x] Each item in the log array that is returned from GET /api/users/:_id/logs is an object that should have a description, duration, and date properties.
+
+ - [x] The description property of any object in the log array that is returned from GET /api/users/:_id/logs should be a string.
+ - [x] The duration property of any object in the log array that is returned from GET /api/users/:_id/logs should be a number.
+ - [x] The date property of any object in the log array that is returned from GET /api/users/:_id/logs should be a string. Use the dateString format of the Date API.
+
+ - [ ] You can add from, to and limit parameters to a GET /api/users/:_id/logs request to retrieve part of the log of any user. from and to are dates in yyyy-mm-dd format. limit is an integer of how many logs to send back.
+
+ - [ ] You should provide your own project, not the example URL.
+*/
+app.get('/api/users/:_id/logs', (req, res) => {
+  const { _id } = req.params;
+
+  const user = getUser(_id);
+  const log = createLog(_id);
+
+  res.json({ ...user, count: log.length, log });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
